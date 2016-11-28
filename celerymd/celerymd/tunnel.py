ef9faa026e0bf056d7228e5b222a133accb8f597
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-11-28 14:46:39 +0100

    better SSH Tunnel

--- celerymd/celerymd/tunnel.py
+++ celerymd/celerymd/tunnel.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from sshtunnel import SSHTunnelForwarder
+# from sshtunnel import SSHTunnelForwarder
 import subprocess
 import os
 
@@ -11,69 +11,41 @@ tunnel_server = None
 tunnel_process = None
 
 
-def create_server(
-        redis_server,
-        local_node_port,
-        ssh_password=None,
-        ssh_host_key=None,
-):
-    global tunnel_server
-    logger.info('create tunnel object')
-
-    reg_ex = re.compile('([a-zA-Z0-9_]+)@([a-zA-Z0-9\.\-\_]+):([0-9]+)')
-
-    redis_server_user, redis_server, redis_port = \
-        reg_ex.findall(redis_server)[0]
-
-    tunnel_server = SSHTunnelForwarder(
-        redis_server,
-        ssh_username=redis_server_user,
-        ssh_password=ssh_password,
-        local_bind_address=('127.0.0.1', local_node_port),
-        remote_bind_address=('127.0.0.1', int(redis_port))
-    )
-
-
-def open_tunnel(**kwargs):
-    global tunnel_server
-    logger.info('Create tunnel')
-    tunnel_server.start()
-    logger.info('tunnel established at port %s' % str(tunnel_server.local_bind_address))
-
-
-def close_tunnel(**kwargs):
-    global tunnel_server
-    logger.info('Shut down tunnel')
-    tunnel_server.stop()
-    logger.info('Tunnel removed')
-
-
-def bash_tunnel_open(
-        redis_server,
-        local_node_port):
-
-    global tunnel_process
-
-    reg_ex = re.compile('([a-zA-Z0-9_]+)@([a-zA-Z0-9\.\-\_]+):([0-9]+)')
-
-    redis_server_user, redis_server, redis_port = \
-        reg_ex.findall(redis_server)[0]
-
-    # FNULL = open(os.devnull, 'w')
-
-    bashCommand = " ssh -N -L%d:localhost:%d %s@%s" % (
-        local_node_port, int(redis_port), redis_server_user, redis_server
-    )
-
-    tunnel_process = subprocess.Popen(
-        bashCommand.split()
-    )
-
-
-def bash_tunnel_close():
-    global tunnel_process
-
-    tunnel_process.kill()
+# def create_server(
+#         redis_server,
+#         local_node_port,
+#         ssh_password=None,
+#         ssh_host_key=None,
+# ):
+#     global tunnel_server
+#     logger.info('create tunnel object')
+#
+#     reg_ex = re.compile('([a-zA-Z0-9_]+)@([a-zA-Z0-9\.\-\_]+):([0-9]+)')
+#
+#     redis_server_user, redis_server, redis_port = \
+#         reg_ex.findall(redis_server)[0]
+#
+#     tunnel_server = SSHTunnelForwarder(
+#         redis_server,
+#         ssh_username=redis_server_user,
+#         ssh_password=ssh_password,
+#         local_bind_address=('127.0.0.1', local_node_port),
+#         remote_bind_address=('127.0.0.1', int(redis_port))
+#     )
+#
+#
+# def open_tunnel(**kwargs):
+#     global tunnel_server
+#     logger.info('Create tunnel')
+#     tunnel_server.start()
+#     logger.info('tunnel established at port %s' % str(tunnel_server.local_bind_address))
+#
+#
+# def close_tunnel(**kwargs):
+#     global tunnel_server
+#     logger.info('Shut down tunnel')
+#     tunnel_server.stop()
+#     logger.info('Tunnel removed')
 
 
 class BashTunnel(object):
